9dc7e94e59a9c853fbb3f022b535874d43e7aef5
jessicaferns123 <fernandesjessica764@gmail.com>
2025-04-13 18:07:43 +0400

    first-react-app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { FirstComponent } from './components/FirstComponent'
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <FirstComponent />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/FirstComponent.jsx
@@ -0,0 +1,15 @@
+export const FirstComponent = () => {
+    return(
+        <>
+            <h1>This is First Component</h1>
+        </>
+    )
+}
+
+export const SecondComponent = () => {
+    return(
+        <>
+            <h1>This is Second Component</h1>
+        </>
+    )
+}
